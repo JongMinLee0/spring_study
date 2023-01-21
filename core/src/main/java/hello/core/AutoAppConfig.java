4d31f718ac917e76acf95f4b1b99da274fd3b6b6
Jongmin Lee <jongminlee0218@gmail.com>
2023-01-21 16:35:13 +0900

    add: 컴포넌트 스캔 - 필터, 중복 등록과 충돌

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //basePackages = "hello.core.member",
+        //basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
+// 지정하지 않으면 @ComponentScan이 붙은 하위 패키지를 모두 뒤진다
 public class AutoAppConfig {
 
 }
