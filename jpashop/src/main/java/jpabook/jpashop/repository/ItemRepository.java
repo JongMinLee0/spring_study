c4380f8c2e9b7b590e48258c2b38a1848ee29f11
DESKTOP-J5LDJ1O\user <jongminlee0218@gmail.com>
2023-02-22 18:13:23 +0900

    ãadd: 상품 엔터티 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+
+    }
+}
